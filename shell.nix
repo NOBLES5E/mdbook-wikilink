c8fb24f64164fb61829de3c5cce4ea519801fbd5
Victor Freire <freirel.victor@gmail.com>
2021-02-15 22:27:58 -0300

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
+  mdbook-wikilink = import ./default.nix;
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     rustc
     cargo
+    mdbook-wikilink
   ];
 }
