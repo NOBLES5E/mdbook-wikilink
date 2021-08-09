384ea543e80f4c8ca465ecf8f05fc82745a9bc8a
Victor Freire <victor@freire.dev.br>
2021-08-09 14:46:05 -0300

    pinned rust compiler version

--- flake.nix
+++ flake.nix
@@ -1,45 +1,60 @@
 {
   description = "Support for wikilinks on mdBook";
 
+
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
+    rust.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+  outputs = { self, nixpkgs, utils, naersk, rust }:
     utils.lib.eachDefaultSystem
-      (system:
-        let
-          name = "mdbook-wikilink";
+      (
+        system:
+          let
+            name = "mdbook-wikilink";
 
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+            rust-overlay = import rust;
 
-          naersk-lib = naersk.lib."${system}";
-        in
-        rec {
-          # `nix build`
-          packages.${name} = naersk-lib.buildPackage {
-            pname = name;
-            root = ./.;
-          };
-          defaultPackage = packages.${name};
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ rust-overlay ];
+            };
 
-          # `nix run`
-          apps.${name} = utils.lib.mkApp {
-            drv = packages.${name};
-          };
-          defaultApp = apps.${name};
+            rust-pkg = pkgs.rust-bin.stable."1.54.0".default.override {
+              extensions = [
+                "rust-std"
+              ];
+            };
 
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              rustc
-			  rustfmt
-              cargo
-              rust-analyzer
-            ];
-          };
-        });
+            naersk-lib = naersk.lib."${system}".override {
+              rustc = rust-pkg;
+            };
+
+            mdzk-pkg = naersk-lib.buildPackage {
+              pname = name;
+              root = pkgs.lib.cleanSource ./.;
+            };
+          in
+            rec {
+              # `nix build`
+              packages.${name} = mdzk-pkg;
+              defaultPackage = packages.${name};
+
+              # `nix run`
+              apps.${name} = utils.lib.mkApp {
+                drv = packages.${name};
+              };
+              defaultApp = apps.${name};
+
+              # `nix develop`
+              devShell = pkgs.mkShell {
+                nativeBuildInputs = with pkgs; [
+                  rust-pkg
+                  rust-analyzer
+                ];
+              };
+            }
+      );
 }
